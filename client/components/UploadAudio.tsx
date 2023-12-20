7d03694eb265750df0a2066412a08db97d14d34b
Ian Turner <eze@Ians-MacBook-Pro.local>
2023-12-20 13:04:09 -0700

    add button and style

--- client/components/UploadAudio.tsx
+++ client/components/UploadAudio.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Button, Alert } from 'react-native';
-import { styled } from 'nativewind';
+import React, { useState } from 'react';
+import { TouchableOpacity, Alert, Text, View } from 'react-native';
+import { styled, tw } from 'nativewind';
 import DocumentPicker from 'react-native-document-picker';
 import RNFetchBlob from 'rn-fetch-blob';
 import axios from 'axios';
 
-const StyledButton = styled(Button);
+const StyledTouchableOpacity = styled(TouchableOpacity);
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 const UploadAudio: React.FC = () => {
+  const [uploadedSong, setUploadedSong] = useState<string | null>(null);
+
   const handleSelectFile = async () => {
     try {
       const res = await DocumentPicker.pick({
@@ -39,14 +43,38 @@ const UploadAudio: React.FC = () => {
       const response = await axios.post('http://localhost:8000/audio/upload/', formData);
       console.log('Upload response:', response.data);
 
+      if (response.data && response.data.message === 'File uploaded successfully!') {
+        setUploadedSong(pickedFile.name);
+      }
+
     } catch (err) {
       console.error('Error during file selection or upload:', err);
       Alert.alert('Error', 'An error occurred while picking the file.');
     }
   };
 
+  const handleGenerateDrumChart = () => {
+    console.log('Generating drum chart for', uploadedSong);
+  };
+
   return (
-    <StyledButton title="Upload Audio" onPress={handleSelectFile} />
+    <StyledView className="flex-1 justify-center items-center p-4">
+      <StyledTouchableOpacity title="Upload Audio" onPress={handleSelectFile} tw="bg-4 bg-blue-500 py-2 px-4 rounded">
+        <StyledText tw="text-white font-bold">Upload Audio</StyledText>
+      </StyledTouchableOpacity>
+      {uploadedSong && (
+        <>
+          <StyledView className="mt-4 p-2 bg-slate-700 rounded-lg">
+            <StyledText className="text-slate-300 text-lg font-semibold">
+              {uploadedSong}
+            </StyledText>
+          </StyledView>
+          <StyledTouchableOpacity onPress={handleGenerateDrumChart} tw="mt-4 bg-rose-400 text-rose-100 font-bold py-2 px-4 rounded">
+            <StyledText tw="text-rose-100 font-bold">Generate Drum Chart</StyledText>
+          </StyledTouchableOpacity> 
+        </>
+      )}
+    </StyledView>
   );
 };
 
